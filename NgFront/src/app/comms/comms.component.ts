ee324f3b4f528540e6428cb9bf174b483ef31f8b
kajih <kajih@mimiru>
2023-01-06 18:57:35 +0100

    Updated angular frontend

--- NgFront/src/app/comms/comms.component.ts
+++ NgFront/src/app/comms/comms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { catchError, retry } from 'rxjs/operators';
 
 @Component({
@@ -11,13 +12,14 @@ import { catchError, retry } from 'rxjs/operators';
 export class CommsComponent implements OnInit {
 
   // myWebSocket: WebSocketSubject = webSocket('ws://localhost:8000');
-  data :string = "";
+  data: string = '';
+  ws: WebSocketSubject<string> | null = null;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.data = "SET in ngOnInit"
-    this.http.get("http://localhost:8080", { responseType: 'text' })
+    this.data = 'SET in ngOnInit';
+    this.http.get('http://localhost:8080', { responseType: 'text' })
       .subscribe((resp:string) => {
         console.log(resp);
         this.data = resp.toString();
@@ -26,6 +28,11 @@ export class CommsComponent implements OnInit {
 
   onPush() {
     console.log("Pushed it");
+    this.ws = webSocket<string>('ws://localhost:8080/ws');
+    this.ws.subscribe(
+        (msg: string) => { this.data = msg; },
+        err => { console.log('WebSocket Error'); },
+        () => { console.log('WebSocket Closed'); }
+      ); 
   }
 }
-
